faee8301325f380fefafbe1b863266360c7745a5
Your Name <you@example.com>
2021-05-19 12:53:26 +0800

    Merge summary part

---
+++ scripts/Summary information.R
@@ -0,0 +1,82 @@
+
+# Read files
+games <- read.csv("../data/games.csv", stringsAsFactors = FALSE)
+
+# Filter data to 2019 season
+NBA_games_2019 <- games %>% filter(SEASON == "2019")
+
+
+### Summary information: Does home or away, different environment and audiences, affect players' shooting percentage? 
+### FG_PCT Home vs. Away
+
+Ave_FG_PTC_home <- NBA_games_2019 %>%
+  select(FG_PCT_home) %>%
+  summarize(
+    count = n(),
+    FG_PCT_home = sum(FG_PCT_home),
+    ave = FG_PCT_home / count)%>%
+  pull(ave)
+Ave_FG_PTC_home <- round(Ave_FG_PTC_home, digits = 3)
+
+Ave_FG_PTC_away <- NBA_games_2019 %>%
+  select(FG_PCT_away) %>%
+  summarize(
+    count = n(),
+    FG_PCT_away = sum(FG_PCT_away),
+    ave = FG_PCT_away / count) %>%
+  pull(ave)
+Ave_FG_PTC_away <- round(Ave_FG_PTC_away, digits = 3)
+
+
+### FG3_PCT Home vs. Away
+Ave_FG3_PCT_home <- NBA_games_2019 %>%
+  select(FG3_PCT_home) %>%
+  summarize(
+    count = n(),
+    FG3_PCT_home = sum(FG3_PCT_home),
+    ave = FG3_PCT_home / count) %>%
+  pull(ave)
+Ave_FG3_PCT_home <- round(Ave_FG3_PCT_home, digits = 3)
+
+Ave_FG3_PCT_away <- NBA_games_2019 %>%
+  select(FG3_PCT_away) %>%
+  summarize(
+    count = n(),
+    FG3_PCT_away = sum(FG3_PCT_away),
+    ave = FG3_PCT_away / count) %>%
+  pull(ave)
+Ave_FG3_PCT_away <- round(Ave_FG3_PCT_away, digits = 3)
+
+# total number of win and lose
+num_home_win <- NBA_games_2019 %>%
+  select(HOME_TEAM_WINS) %>%
+  filter(HOME_TEAM_WINS == "1") %>%
+  summarize(total_win = n()) %>%
+  pull(total_win)
+
+num_home_loose <- NBA_games_2019 %>%
+  select(HOME_TEAM_WINS) %>%
+  filter(HOME_TEAM_WINS == "0") %>%
+  summarize(total_loose = n()) %>%
+  pull(total_loose)
+
+### Rebound Home vs. Away
+Rebound_home <- NBA_games_2019 %>%
+  select(REB_home) %>%
+  summarize(
+    count = n(),
+    REB_home = sum(REB_home),
+    ave = REB_home / count) %>%
+  pull(ave)
+Rebound_home <- round(Rebound_home, digits = 3)
+
+
+Rebound_away <- NBA_games_2019 %>%
+  select(REB_away) %>%
+  summarize(
+    count = n(),
+    REB_away = sum(REB_away),
+    ave = REB_away / count) %>%
+  pull(ave)
+Rebound_away <- round(Rebound_away, digits = 3)
+
---
+++ scripts/Summary table.R
@@ -0,0 +1,54 @@
+### Summary table
+
+# load and clean data sets
+games_data <- read.csv("../data/games.csv", stringsAsFactors = FALSE)
+games_data_2020 <- games_data %>% 
+  filter(SEASON == "2020")
+teams_data <- read.csv("../data/teams.csv", stringsAsFactors = FALSE)
+teams_data <- teams_data %>% 
+  select(TEAM_ID, NICKNAME)
+
+# add team names for home teams
+teams_data <- teams_data %>% 
+  rename(HOME_TEAM_ID = TEAM_ID)
+add_home_team_name <- 
+  left_join(games_data_2020, teams_data, by = "HOME_TEAM_ID") %>% 
+  rename(HOME_TEAM_NAME = NICKNAME)
+
+# add team names for visitor teams
+teams_data <- teams_data %>% 
+  rename(VISITOR_TEAM_ID = HOME_TEAM_ID)
+games_data <-
+  left_join(add_home_team_name, teams_data, by = "VISITOR_TEAM_ID") %>% 
+  rename(VISITOR_TEAM_NAME = NICKNAME)
+
+
+
+# Table
+all_teams_data <- games_data %>%
+  select(HOME_TEAM_NAME, REB_home, FT_PCT_home, FG_PCT_home) %>%
+  group_by(HOME_TEAM_NAME) %>%
+  summarize(REB_home = sum(REB_home),
+            FT_PCT_home = sum(FT_PCT_home),
+            FG_PCT_home = sum(FG_PCT_home)) %>%
+  arrange(FG_PCT_home) 
+
+  
+# Table with 4 teams
+offseason_teams_data <- games_data %>%
+  select(HOME_TEAM_NAME, REB_home, FT_PCT_home, FG_PCT_home) %>%
+  group_by(HOME_TEAM_NAME) %>%
+  filter(HOME_TEAM_NAME %in% c("Warriors", "Raptors", "Trail Blazers", "Bucks")) %>%
+  summarize(REB_home = sum(REB_home),
+            FT_PCT_home = sum(FT_PCT_home),
+            FG_PCT_home = sum(FG_PCT_home))
+
+  
+
+  
+  
+  
+  
+  
+  
+  
