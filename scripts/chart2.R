18fea48c2713274d54e69df1857198c0589c98e3
unknown <rwu1227@gmail.com>
2021-05-20 17:54:07 -0700

    fix the error code on index

--- scripts/chart2.R
+++ scripts/chart2.R
@@ -1,83 +1,10 @@
-<<<<<<< HEAD
-games <- read.csv("../data/games.csv", stringsAsFactors = FALSE)
-
-library(dplyr)
-library(ggplot2)
-library(plotly)
-library(lintr)
-
-=======
->>>>>>> 356ae48af027f5463cd919e7279a9f2858002b75
 # Scatter Plot: Lakers 3 Point Percentage vs League Average
 # load needed packages
 
 chart2 <- function(games_data) {
-<<<<<<< HEAD
-<<<<<<< HEAD
 
-NBA_games_2020 <- games %>% filter(SEASON == "2019")
-
-# Filter for all Lakers games
-lakers_home_games <- filter(NBA_games_2020, HOME_TEAM_ID == "1610612747")
-
-lakers_away_games <- filter(NBA_games_2020, VISITOR_TEAM_ID == "1610612747")
-
-lakers_games <- full_join(lakers_away_games, lakers_home_games)
-
-# Compute league averages for 3 point percentage and points
-ave_fg3_pct <- NBA_games_2020 %>%
-  select(FG3_PCT_home, FG3_PCT_away) %>%
-  summarize(
-    count = n() * 2,
-    FG3_PCT_home = sum(FG3_PCT_home),
-    FG3_PCT_away = sum(FG3_PCT_away),
-    ave = (FG3_PCT_home + FG3_PCT_away) / count) %>%
-  pull(ave)
-ave_fg3_pct <- round(Ave_FG3_PCT, digits = 3)
-
-ave_pts <- NBA_games_2020 %>%
-  select(PTS_home, PTS_away) %>%
-  summarize(
-    count = n() * 2,
-    PTS_home = sum(PTS_home),
-    PTS_away = sum(PTS_away),
-    ave = (PTS_home + PTS_away) / count) %>%
-  pull(ave)
-ave_pts <- round(Ave_PTS, digits = 3)
-
-# combine FG3_PCT and PTS for home and away games to use as x and y axes
-fg3 <- c(lakers_home_games$FG3_PCT_home, lakers_away_games$FG3_PCT_away)
-
-pts <- c(lakers_home_games$PTS_home, lakers_away_games$PTS_away)
-
-# create "colors" vector for Legend in scatterplot
-colors <- c("Lakers Games" = "black", "League Average" = "red")
-
-# create a scatterplot comparing the League Average 
-# (3 Point Percentage and Points) to the stats in each Lakers Game
-lakers_3pt <- ggplot(data = lakers_games) +
-  geom_point(mapping = aes(x = FG3_PCT, y = PTS, color = "Lakers Games")) +
-  geom_point(mapping = aes(x = Ave_FG3_PCT, y = Ave_PTS,
-                           color = "League Average")) +
-  labs(y = "Points", x = "3 pt pct") +
-  labs(title = "Lakers 3 Point Percentage:Pionts vs League Average") +
-  scale_color_manual(values = colors, guide = FALSE)
-
-# Make scatterplot interactive
-lakers_fg3_pct_vs_league <- ggplotly(lakers_3pt) 
-
-return(lakers_fg3_pct_vs_league)
-}
-
-lint("chart2.R")
-=======
-  
-  NBA_games_2020 <- games %>% filter(SEASON == "2019")
-  
-=======
   nba_games_2020 <- games_data %>% filter(SEASON == "2019")
 
->>>>>>> d6fd3660c0fb0be48b712a09f79d9893a796f6f1
   # Filter for all Lakers games
   lakers_home_games <- filter(nba_games_2020, HOME_TEAM_ID == "1610612747")
   lakers_away_games <- filter(nba_games_2020, VISITOR_TEAM_ID == "1610612747")
@@ -125,9 +52,6 @@ lint("chart2.R")
   # Make scatterplot interactive
   lakers_fg3_pct_vs_league <- ggplotly(lakers_3pt)
 
-<<<<<<< HEAD
->>>>>>> 356ae48af027f5463cd919e7279a9f2858002b75
-=======
   return(lakers_fg3_pct_vs_league)
 }
->>>>>>> d6fd3660c0fb0be48b712a09f79d9893a796f6f1
+
