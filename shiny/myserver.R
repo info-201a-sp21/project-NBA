72814729f6661a494f21e4f28779de17cbf16260
unknown <rwu1227@gmail.com>
2021-06-01 22:16:14 -0700

    add table to summary

--- shiny/myserver.R
+++ shiny/myserver.R
@@ -134,5 +134,24 @@ server <- function(input, output) {
     return(lakers_pts_chart)
     
   })
+  
+  #summary table
+  output$summary <-renderTable({
+    
+    # Table
+    all_teams_data <- games_data %>%
+      select(HOME_TEAM_NAME, REB_home, FT_PCT_home, FG_PCT_home) %>%
+      group_by(HOME_TEAM_NAME) %>%
+      summarize(
+        Rebound = sum(REB_home),
+        Free_Throw_Percentage = sum(FT_PCT_home),
+        Field_Goal_Percentage = sum(FG_PCT_home)
+      ) %>%
+      arrange(Field_Goal_Percentage)
+    names(all_teams_data)[1:4] <- c("Team Name", "Rebounds", "Free Throw %",
+                                    "Field Goal %" )
+    
+    return(all_teams_data)
+  })
 }
 
--- shiny/myui.R
+++ shiny/myui.R
@@ -1,72 +1,6 @@
 library(shiny)
 library(plotly)
 
-# chart1 code
-# select input
-x_input <- selectInput(
-  inputId = "team_name",
-  label = "Select one of the TOP8 teams:",
-  choices = list("Bucks", "Heat", "Jazz", "Lakers", "Nuggets", "Pacers", 
-                 "Pelicans", "Spurs"),
-  selected = "Bucks"
-)
-
-# sidebar
-chart1_sidebar <- sidebarPanel(
- x_input,
- p("Note: Top8 teams are measured by average FG percentage")
-)
-
-# show plot
-chart1_plot <- mainPanel(
-  h4("This chart was intended to visualize and compare how top 8 teams who
-     have the highest Field Goal percentage in general perform in their home
-     and away games. The grouped bar chart shows clearly the difference
-     between each team¡¯s Field Goal percentage in their home and away games."),
-  plotOutput("chart1")
-)
-
-chart1_panel <- tabPanel(
-  "Chart 1",
-  titlePanel("Does Home and Away game affect TOP8 teams FG percentage?"),
-  hr(),
-  sidebarLayout(
-    chart1_sidebar,
-    chart1_plot
-  )
-)
-
-# chart_3 code
-# sidebar
-chart_3_sidebar <- sidebarPanel(
-  sliderInput(
-    inputId = "min_score",
-    label = "Lakers' Score above", min = 77, max = 142, value = 105
-  )
-)
-
-# show the plot
-chart_3_plot <- mainPanel(
-  h4("This chart is trying to figure out how many points the 2019 champion,
-      Lakers, get for each game throughout the whole season. Is the champion
-      team perform steady? The red line is the mean PTS from selected range,
-      so the more closer to the red line, the more steady they perform."),
-  plotlyOutput("chart3")
-)
-
-chart_3_panel <- tabPanel(
-  "Champion Team",
-  tags$p(
-    id = "titlePanel",
-    "Does Champion team's (Lakers) perform steady throughout season 2019?"
-  ),
-  hr(),
-  sidebarLayout(
-    chart_3_sidebar,
-    chart_3_plot
-  )
-)
-
 # Introduction Page
 introduction <- fluidPage(
   h1("NBA Data Analytics"), 
@@ -99,6 +33,78 @@ introduction <- fluidPage(
     throughout the whole season? Is the champion team perform steady?")
 )
 
+# chart1 code
+# select input
+x_input <- selectInput(
+  inputId = "team_name",
+  label = "Select one of the TOP8 teams:",
+  choices = list("Bucks", "Heat", "Jazz", "Lakers", "Nuggets", "Pacers", 
+                 "Pelicans", "Spurs"),
+  selected = "Bucks"
+)
+
+# sidebar
+chart1_sidebar <- sidebarPanel(
+ x_input,
+ p("Note: Top8 teams are measured by average FG percentage")
+)
+
+# show plot
+chart1_plot <- mainPanel(
+  h4("This chart was intended to visualize and compare how top 8 teams who
+     have the highest Field Goal percentage in general perform in their home
+     and away games. The grouped bar chart shows clearly the difference
+     between each team¡¯s Field Goal percentage in their home and away games."),
+  br(),
+  plotOutput("chart1")
+)
+
+chart1_panel <- tabPanel(
+  "Chart 1",
+  titlePanel("Does Home and Away game affect TOP8 teams FG percentage?"),
+  hr(),
+  sidebarLayout(
+    chart1_sidebar,
+    chart1_plot
+  )
+)
+
+# chart_3 code
+# sidebar
+chart_3_sidebar <- sidebarPanel(
+  sliderInput(
+    inputId = "min_score",
+    label = "Lakers' Score above", min = 77, max = 142, value = 105
+  )
+)
+
+# show the plot
+chart_3_plot <- mainPanel(
+  h4("This chart is trying to figure out how many points the 2019 champion,
+      Lakers, get for each game throughout the whole season. Is the champion
+      team perform steady? The red line is the mean PTS from selected range,
+      so the more closer to the red line, the more steady they perform."),
+  br(),
+  plotlyOutput("chart3")
+)
+
+chart_3_panel <- tabPanel(
+  "Champion Team",
+  tags$p(
+    id = "titlePanel",
+    "Does Champion team's (Lakers) perform steady throughout season 2019?"
+  ),
+  hr(),
+  sidebarLayout(
+    chart_3_sidebar,
+    chart_3_plot
+  )
+)
+
+summary <- mainPanel(
+  h4("summary table"),
+  tableOutput("summary")
+)
 
 ui <- fluidPage(
   includeCSS("style.css"),
@@ -107,7 +113,7 @@ ui <- fluidPage(
              tabPanel("TOP 8 TEAMS", chart1_panel),
              tabPanel("Chart 2", "content"),
              tabPanel("CHAMPION TEAM", chart_3_panel),
-             tabPanel("SUMMARY INFORMATION")
+             tabPanel("SUMMARY INFORMATION", summary)
   )
 )
   
